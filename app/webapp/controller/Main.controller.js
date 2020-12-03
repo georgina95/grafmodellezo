6b787d1258a8b7664d88c33fbe3a96795cae8c9f
georgina95 <georgina95@gmail.com>
2020-12-03 07:58:21 +0000

    change odata version to v2

--- app/webapp/controller/Main.controller.js
+++ app/webapp/controller/Main.controller.js
@@ -1,12 +1,12 @@
 sap.ui.define([
-	"grafmodellezo/app/controller/BaseController"
+    "grafmodellezo/app/controller/BaseController"
 ], function (BaseController) {
-	"use strict";
+    "use strict";
 
-	return BaseController.extend("grafmodellezo.app.controller.Main", {
+    return BaseController.extend("grafmodellezo.app.controller.Main", {
 
-		onInit: function () {
-			var oModel = new sap.ui.model.json.JSONModel({
+        onInit: function () {
+            var oModel = new sap.ui.model.json.JSONModel({
                 Tweets: [
                     {
                         text: "First"
@@ -22,10 +22,24 @@ sap.ui.define([
 
             this.getView().setModel(oModel, "Twitter");
         },
-        
-        onPress: function(oEvent) {
-            
+
+        onPress: function (oEvent) {
+            var oModel = this.getOwnerComponent().getModel("catalogModel");
+            /*
+                oModel.read("/Authors", {
+                    success: jQuery.proxy(this.onSuccess, this),
+                    error: this.onError
+                })
+            */
+        },
+
+        onSuccess: function (oData) {
+            sap.m.MessageToast("Success: " + oData.results.length);
+        },
+
+        onError: function (oError) {
+            sap.m.MessageToast("Error");
         }
-	});
+    });
 
 });
